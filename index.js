bbc2ca7273b8ad2b2d9f972ecd587ae08029a70d
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-08-26 18:54:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+
+  }
+};
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === skipValue) {
+      return array[i];
+    }
+  }
+};
+
+function findBy(array, findFn) {
+
+};
