e96c6b1a762e8443e81dc0357241ee10e8cd1f47
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-08-26 19:00:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
 
   }
-};
+}
 
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === skipValue) {
       continue
-} else {
-  array[i] = changeValue;
-}
+    } else {
+      array[i] = changeValue;
+  }
 }
 
 function findBy(array, findFn) {
 
-};
+}
