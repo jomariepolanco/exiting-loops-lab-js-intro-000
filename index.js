0f6f148480f6a01107dab0931398919f83bbc9dc
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-08-26 19:04:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
-    if(array[i] < stopValue) {
+    if (array[i] === stopValue) {
+      return array;
+    }  else {
       array[i] = changeValue;
-    } else if (array[i] >= stopValue) {
-      break
     }
   }
-}
+}  
 
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length; i++) {
