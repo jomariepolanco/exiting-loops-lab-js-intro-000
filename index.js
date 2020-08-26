d03812aa0665e3e28a1ce76d24ded2a1c0fe3e4e
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-08-26 18:56:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function breakOut(array, changeValue, stopValue) {
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length; i++) {
     if(array[i] === skipValue) {
-      return array[i];
+      return array;
     } else {
       return changeValue;
     }
